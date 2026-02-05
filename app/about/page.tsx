5874fa6cd6984c25005443b25a7c76d3c37d3816
Efe <48efe43@gmail.com>
2026-02-05 21:11:48 +0100

    remove showButton prop

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
       <HeroSection
         title="Hakkımızda"
         subtitle="Pudi Butik hikayesi"
-        showButton={false}
+      
       />
 
       <section className="mx-auto max-w-3xl px-4 py-16 sm:px-6 lg:px-8">
